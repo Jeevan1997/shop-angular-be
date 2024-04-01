465e117e466e985e6267a26537ef3ea5a86611cb
SaiJeevan Kodakanla <SaiJeevan_Kodakanla@epam.com>
2024-04-01 18:02:53 +0530

    added the schema validations

--- product-service/src/functions/create-product/handler.ts
+++ product-service/src/functions/create-product/handler.ts
@@ -3,24 +3,32 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
 import { ProductService } from '@libs/services/product.service';
-import { FromSchema } from 'json-schema-to-ts';
+import { CreateProductSchema } from '@libs/validators/product-schema';
 
 const createProduct: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  
-  const body: FromSchema<typeof schema> = event.body;
 
-  if (!body) {
-    return formatJSONResponse({ error: "Missing body" });
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  const productData = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+  const { error } = CreateProductSchema.validate(productData);
+  if (error) {
+    console.error('Validation error:', error.details);
+    return formatJSONResponse({ statusCode:400, error: "Invalid request body" });
   }
 
+  console.log('Parsed and validated product data:', productData);
   const productService = new ProductService();
-  const product = await productService.createProduct(body);
-  if(!product){
-    return formatJSONResponse({ error: "Error in creating a product" });
+  try{
+    console.log('Attempting to create product and stock...');
+    const product = await productService.createProduct(productData);
+    if(!product){
+      return formatJSONResponse({ statusCode:400, error: "Error in creating a product" });
+    }
+    else{
+      return formatJSONResponse({message: "product creation done"});
+    }  
+  }catch(err){
+    return formatJSONResponse({ statusCode:500, error: 'Failed to create product' });
   }
-  else{
-    return formatJSONResponse({message: "product creation done"});
-  }  
 };
 
 export const main = middyfy(createProduct);
