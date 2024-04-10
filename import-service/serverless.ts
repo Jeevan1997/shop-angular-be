935b689ac45a4c8ffb68747f6be58a11e063b372
SaiJeevan Kodakanla <SaiJeevan_Kodakanla@epam.com>
2024-04-10 17:40:55 +0530

    task 5 changes done

---
+++ import-service/serverless.ts
@@ -0,0 +1,46 @@
+import type { AWS } from '@serverless/typescript';
+
+import importFileParser from '@functions/import-file-parser';
+import importProductsFile from '@functions/import-products-file';
+
+const serverlessConfiguration: AWS = {
+  service: 'import-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs20.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["s3:PutObject", "s3:GetObject"],
+        Resource: "arn:aws:s3:::file-storage-s3-1/uploaded/*"
+      },
+    ],
+  },
+  // import the function via paths
+  functions: { importProductsFile, importFileParser },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
